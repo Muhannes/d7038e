256e629cc97d761564f0bef036c4e7fad14d90dd
Truls Haraldsson <truhar-3@student.ltu.se>
2017-11-30 10:28:15 +0100

    Lobby client api

---
+++ THT/src/api/LobbySelectionListener.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package api;
+
+import api.models.LobbyRoom;
+
+/**
+ *
+ * @author truls
+ */
+public interface LobbySelectionListener {
+    
+    void notifyLobbySelection(LobbyRoom lobbyRoom);
+}
