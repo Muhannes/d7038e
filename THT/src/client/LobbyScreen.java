912c4d3bf9e90f1252de019d5846e7e70b325f4b
tedjoh-2 <tedjoh-2@student.ltu.se>
2017-12-02 20:27:02 +0100

    Map list

--- THT/src/client/LobbyScreen.java
+++ THT/src/client/LobbyScreen.java
@@ -9,9 +9,26 @@ import com.jme3.app.Application;
 import com.jme3.app.state.AbstractAppState;
 import com.jme3.app.state.AppStateManager;
 import com.jme3.niftygui.NiftyJmeDisplay;
+import com.sun.xml.internal.ws.util.StringUtils;
 import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.NiftyEventSubscriber;
+import de.lessvoid.nifty.controls.Chat;
+import de.lessvoid.nifty.controls.ChatTextSendEvent;
+import de.lessvoid.nifty.controls.ListBox;
+import de.lessvoid.nifty.controls.ListBox.ListBoxViewConverter;
+import de.lessvoid.nifty.controls.ListBoxSelectionChangedEvent;
+import de.lessvoid.nifty.controls.Scrollbar;
+import de.lessvoid.nifty.controls.chatcontrol.ChatEntryModelClass;
+import de.lessvoid.nifty.controls.chatcontrol.builder.ChatBuilder;
+import de.lessvoid.nifty.controls.listbox.ListBoxItemProcessor;
+import de.lessvoid.nifty.controls.listbox.builder.ListBoxBuilder;
+import de.lessvoid.nifty.elements.Element;
 import de.lessvoid.nifty.screen.Screen;
 import de.lessvoid.nifty.screen.ScreenController;
+import de.lessvoid.nifty.tools.SizeValue;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -25,12 +42,14 @@ public class LobbyScreen extends AbstractAppState implements ScreenController{
     private Nifty nifty;
     private NiftyJmeDisplay niftyDisplay;
     private Application app;
+    private Screen screen;
+    private ListBox listBox;
+    private Chat chat;
     
     @Override
     public void initialize(AppStateManager stateManager, Application app){
         LOGGER.log(Level.FINE, "Initializing LoginScreen");
         System.out.println("Init LobbyScreen");
-        
         super.initialize(stateManager, app);        
         this.app = app;
         
@@ -46,8 +65,22 @@ public class LobbyScreen extends AbstractAppState implements ScreenController{
         
         // attach the Nifty display to the gui view port as a processor
         app.getGuiViewPort().addProcessor(niftyDisplay);
-    }
         
+        listBox = screen.findNiftyControl("myListBox", ListBox.class);
+        listBox.addItem("TunnelCrawler <2/5>");
+        listBox.addItem("Run little piggy <3/5>");
+        listBox.addItem("Hungry craws <1/5>");
+    }    
+
+    @NiftyEventSubscriber(id="myListBox")
+    public void onMyListBoxSelectionChanged(final String id, final ListBoxSelectionChangedEvent<String> event) {
+        List<String> selection = event.getSelection();
+        for(String selectedItem : selection) {
+          //Join the lobby here! :D  
+          System.out.println("listbox selection [" + selectedItem + "]");
+        }
+    }
+    
     @Override
     public void cleanup(){
         LOGGER.log(Level.FINE, "Cleanup LoginScreen");
@@ -56,6 +89,8 @@ public class LobbyScreen extends AbstractAppState implements ScreenController{
 
     @Override
     public void bind(Nifty nifty, Screen screen) {
+        this.screen = screen;
+        this.nifty = nifty;
         //TODO: 
     }
 
@@ -78,9 +113,6 @@ public class LobbyScreen extends AbstractAppState implements ScreenController{
         System.out.println("Stopping!");
         app.stop();
     }
-    
-    public String getRoomLobbies(){
-        return "Lobby : 1";
-    }
-    
+        
 }
+
