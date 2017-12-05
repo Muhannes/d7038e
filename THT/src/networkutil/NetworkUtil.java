81df0d07d674710117f7658ea63b442c01be0b58
Truls Haraldsson <truhar-3@student.ltu.se>
2017-12-05 14:38:29 +0100

    Moved network configurations to a new class

--- THT/src/networkutil/NetworkUtil.java
+++ THT/src/networkutil/NetworkUtil.java
@@ -13,9 +13,6 @@ import com.jme3.network.serializing.Serializer;
  * @author hannes
  */
 public class NetworkUtil {
-    public static final int LOBBY_SERVER_PORT = 7999;
-    
-    public static final String SERVER_HOSTNAME = "localhost";
     
     public static void initSerializables(){
         Serializer.registerClass(JoinRoomMessage.class);
