a273d7cd4c3be25bab91cff53caca2150dedc2a6
tedjoh-2 <tedjoh-2@student.ltu.se>
2017-12-11 10:53:45 +0100

    creating static world

--- THT/src/client/SetupState.java
+++ THT/src/client/SetupState.java
@@ -9,7 +9,11 @@ import api.models.Player;
 import com.jme3.app.Application;
 import com.jme3.app.SimpleApplication;
 import com.jme3.app.state.BaseAppState;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
 import com.jme3.niftygui.NiftyJmeDisplay;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.shape.Box;
 import de.lessvoid.nifty.Nifty;
 import network.services.gamesetup.ClientGameSetupService;
 import network.services.gamesetup.GameSetupSessionListener;
@@ -38,7 +42,8 @@ public class SetupState extends BaseAppState implements EventListener{
     
     @Override
     protected void initialize(Application app) {
-        this.app = (SimpleApplication) app;        
+        this.app = (SimpleApplication) app;    
+        
     }
 
     @Override
@@ -63,6 +68,9 @@ public class SetupState extends BaseAppState implements EventListener{
         
         // attach the Nifty display to the gui view port as a processor
         app.getViewPort().addProcessor(niftyDisplay);
+        
+        
+        buildStaticWorld();
     }
 
     @Override
@@ -82,5 +90,15 @@ public class SetupState extends BaseAppState implements EventListener{
             this.setEnabled(false);
             app.getStateManager().getState(GameState.class).setEnabled(true);            
         }
+    }
+    
+    
+    public void buildStaticWorld(){
+        Box b = new Box(1,1,1);
+        Geometry geom = new Geometry("Box", b);
+        Material mat = new Material(app.getAssetManager(), "Common/MatDefs/Misc/Unshaded.j3md");
+        mat.setColor("Color", ColorRGBA.Blue);
+        geom.setMaterial(mat);
+        app.getRootNode().attachChild(geom);
     }
 }
