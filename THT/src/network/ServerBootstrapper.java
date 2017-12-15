938405b4eae8c2782832e3f6693c5d4038c13eb5
Muhannes <haneda-3@student.ltu.se>
2017-12-15 16:40:28 +0100

    merge

--- THT/src/network/ServerBootstrapper.java
+++ THT/src/network/ServerBootstrapper.java
@@ -18,13 +18,10 @@ public class ServerBootstrapper {
     public static void main(String args[]){
         LoginNetworkHandler loginNH = new LoginNetworkHandler();
         LobbyNetworkHandler lobbyNH = new LobbyNetworkHandler();
-        //GameNetworkHandler gameNH = new GameNetworkHandler();
         loginNH.startServer();
         lobbyNH.startServers();
-        //gameNH.startServer();
         NetConfig.networkDelay(30);
         lobbyNH.connectToLoginServer();
-        //gameNH.connectToLobbyServer();
         while (true){
             try {
                 Thread.sleep(1000);
