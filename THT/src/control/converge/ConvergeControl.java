a8d3bd0dce0cec4bd44b6cded85f1a225af5fdc6
Truls Haraldsson <truhar-3@student.ltu.se>
2017-12-28 17:37:15 +0100

    Starting on a convergence control for humans/monster/etc

---
+++ THT/src/control/converge/ConvergeControl.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package control.converge;
+
+import com.jme3.math.Vector3f;
+import com.jme3.renderer.RenderManager;
+import com.jme3.renderer.ViewPort;
+import com.jme3.scene.control.AbstractControl;
+import java.util.List;
+import network.service.movement.MovementSessionListener;
+import network.service.movement.PlayerMovement;
+
+/**
+ * This control will converge a Spatial to a given Setpoint 
+ * @author truls
+ */
+public class ConvergeControl extends AbstractControl implements MovementSessionListener{
+
+    Vector3f setpoint; 
+    // Börvärde in english
+    
+    @Override
+    protected void controlUpdate(float tpf) {
+        // TODO: Do convergence
+    }
+
+    @Override
+    protected void controlRender(RenderManager rm, ViewPort vp) {
+        // Nothing
+    }
+
+    @Override
+    public void newMessage(List<PlayerMovement> playerMovements) {
+        for(PlayerMovement pm : playerMovements){
+            if(pm.id.equals(getSpatial().getName())){
+                setpoint = pm.location;
+                return;
+            }
+        }
+    }
+    
+}
